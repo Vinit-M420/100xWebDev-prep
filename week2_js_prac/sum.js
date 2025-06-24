3409eeada605026976f42ce3727a08d958f6da7e
Vinit <vinit_motghare@outlook.com>
2025-06-24 19:49:47 +0530

    first commit

---
+++ week2_js_prac/sum.js
@@ -0,0 +1,7 @@
+function sum(a,b){
+    return a+b
+}
+
+let num = sum(2,5)
+
+console.log(num)
---
+++ week2_js_prac/sumtonum.js
@@ -0,0 +1,10 @@
+function totalsum(n){
+    let ans = 0;
+    for (let i=1; i<=n; i++) {
+        ans = ans + i
+    }
+    return ans;
+}
+
+console.log(totalsum(10))
+    
