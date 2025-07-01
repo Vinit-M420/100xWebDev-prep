cc910934602a136da6496e14e6cfe2767c764e90
Vinit <vinit_motghare@outlook.com>
2025-07-01 13:59:47 +0530

    Week2 assignment done

--- javascript_week2/week-2-js-assignment/hard/calculator.js
+++ javascript_week2/week-2-js-assignment/hard/calculator.js
@@ -8,7 +8,7 @@
     - clear: makes the `result` variable to 0
     - getResult: returns the value of `result` variable
     - calculate: takes a string expression which can take multi-arithmetic operations and give its result
-      example input: `10 +   2 *    (   6 - (4 + 1) / 2) + 7`
+      example input: `10 +   2 *  (   6 - (4 + 1) / 2) + 7`
       Points to Note: 
         1. the input can have multiple continuous spaces, you're supposed to avoid them and parse the expression correctly
         2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
@@ -16,6 +16,60 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+    constructor() {
+        this.result = 0;
+    }
+
+    add(number){
+      this.result += number;
+    }
+
+    subtract(number){
+      this.result -= number;
+    }
+
+    multiply(number){
+      this.result *= number;
+    }
+
+    divide(number){
+      if (number===0){
+        throw error('Cant divide with zero');
+      }
+      else{
+        this.result /= number;
+      }
+    }
+
+    clear(){
+      this.result = 0
+    }
+
+    getResult(){
+      return this.result;
+    }
+
+    calculate(input){
+      const temp = input;
+      const cleanedExpression = temp.replace(/\s+/g, '');
+      const isValidExpression = /^[0-9+\-*/().]+$/.test(cleanedExpression);
+
+      if (!isValidExpression) {
+        throw new Error("Invalid expression.");
+      }
+      try{
+        this.result = eval(cleanedExpression);
+      }
+      catch(error){
+         throw new Error("Invalid expression.");
+      }
+      if (this.result === Infinity) {
+        throw new Error("Cannot divide a number by 0.");
+      }
+    
+      return this.result;
+    }
+}
 
 module.exports = Calculator;
