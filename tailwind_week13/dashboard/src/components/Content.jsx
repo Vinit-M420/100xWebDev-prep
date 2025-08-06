b538fa67eda501eb9a1170c930b674e5195cc3db
Vinit <vinit_motghare@outlook.com>
2025-08-06 19:31:12 +0530

    Added icons and worked on Main content

--- tailwind_week13/dashboard/src/components/Content.jsx
+++ tailwind_week13/dashboard/src/components/Content.jsx
@@ -1,11 +1,35 @@
-import cosmic from '../assets/Cosmic.png';
+import bokuto from '../assets/bokuto.jpg';
 
 export const Content = () => {
 
     return (
     <>
         <div className="bg-gray-800 h-screen w-full">
-            <img src={cosmic} alt="my pic" className='absolute h-50 w-full'/>
+            <div className='h-40 w-full bg-black'/>
+        
+
+        <div className="grid grid-cols-11 px-10">
+            <div className="h-80 mr-5 rounded-2xl shadow md:col-span-3 bg-white -translate-y-10 
+                flex flex-col items-center justify-center text-center py-8 px-4 col-span-11">
+                <img src={bokuto} alt="my pic" className="rounded-xl w-30 h-30 shadow-md"/>
+                <h2 className="font-bold text-xl my-3">Vinit M</h2>
+                <p className="text-gray-500">vinit@gmail.com</p>
+                <p className="text-gray-500">9233129345</p>
+                <p className="text-gray-500 mt-3">Mumbai, India</p>
+            </div>
+            <div className="h-124 rounded-2xl shadow md:col-span-5 p-5 text-white col-span-11">
+                <p className='font-medium text-md '>Wednesday, 6 October</p>
+                <h1 className='font-bold text-3xl my-5'>Good Morning, Vinit! 👋</h1>
+                
+                <div className="h-72 rounded-2xl shadow p-5 bg-gray-500">
+
+                </div>
+            </div>
+            <div className="h-72 ml-5 rounded-2xl shadow md:col-span-3 bg-gray-200 translate-y-30 col-span-11">
+                
+            </div>
+
+        </div>
         </div>
     </>
     )
--- tailwind_week13/dashboard/src/components/SideBar.jsx
+++ tailwind_week13/dashboard/src/components/SideBar.jsx
@@ -1,28 +1,36 @@
 import { SideBarHeader } from "./sidebarHeader"
 import home from '../assets/home.svg';
 import settings from '../assets/settings.svg';
+import { SideIcon } from "../icons/SideIcon";
+import webinar from "../assets/community.svg";
+import { Billing } from "../icons/Billing";
+import { Community } from "../icons/Community";
 export const SideBar = () => {
 
     return (
     <>
     <div className="pt-4 px-4 bg-gray-500 h-screen transition-all duration-100 md:w-96 w-0">
-        <SideBarHeader />
+         <div className="my-2 ">
+            <SideIcon/>
+            <SideBarHeader />
+        </div>
+        
         <div className="mt-10 ">
             <div className=" flex justify-between py-2 px-2 rounded-xl mb-4 bg-gray-600 text-white">
-                <h3 className="font-semibold" >Home</h3>
-                <img src={home} alt="home" className='rounded-xl h-6'/>
+                <h3 className="font-semibold">Home</h3>
+                <img src={home} alt="home" className='h-6'/>
             </div>
             <div className="flex justify-between py-2 px-2 rounded-xl mb-4">
                 <h3 className="font-semibold" >Webinars</h3>
-                <img src={home} className='rounded-xl h-6'/>
+                <img src={webinar} alt="webinar" className='h-6'/>
             </div>
             <div className="flex justify-between py-2 px-2 rounded-xl mb-4">
                 <h3 className="font-semibold" >Billings</h3>
-                <img src={home} className='rounded-xl h-6'/>
+                <Billing />
             </div>
             <div className="flex justify-between py-2 px-2  rounded-xl mb-4">
                 <h3 className="font-semibold" >User Management</h3>
-                <img src={home} className='rounded-xl h-6'/>
+                <Community />
             </div>
             <div className="flex justify-between py-2 px-2  rounded-xl mb-4">
                 <h3 className="font-semibold" >Settings</h3>
